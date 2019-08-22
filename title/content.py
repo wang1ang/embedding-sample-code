9b7e1988a124e6da167c82349fb4c49f9bfe26a8
Yang Wang (MM) <yangwa@microsoft.com>
2019-08-22 15:39:06 +0800

    increase decay of content net

--- title/content.py
+++ title/content.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 def hidden_layer(x, dim, scope):
     return tf.layers.dense(x, units=dim, activation=tf.nn.tanh, kernel_initializer=tf.initializers.glorot_normal(), kernel_regularizer=tf.contrib.layers.l2_regularizer(0.0004), name=scope) #tf.keras.initializers.he_normal()
 def output_layer(x, dim, scope, activation):
-    return tf.layers.dense(x, units=dim, activation=activation, kernel_initializer=tf.initializers.glorot_normal(), kernel_regularizer=tf.contrib.layers.l2_regularizer(0.00004), name=scope) #tf.keras.initializers.he_normal()
+    return tf.layers.dense(x, units=dim, activation=activation, kernel_initializer=tf.initializers.glorot_normal(), kernel_regularizer=tf.contrib.layers.l2_regularizer(0.0004), name=scope) #tf.keras.initializers.he_normal()
 def content_net(content, cdssm_dim, mode, activation):
     # content
     layer1 = hidden_layer(content, 4096, 'content/content_fc1')
